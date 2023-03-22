350b2c55799eb2e131109eb19208ef9ed1544874
debryu <116577959+debryu@users.noreply.github.com>
2023-03-22 09:24:23 +0100

    initialized the project

---
+++ config.js
@@ -0,0 +1,10 @@
+// valid on render
+// let host = "https://deliveroojs.onrender.com";
+// let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWFyY28iLCJpYXQiOjE2NzcxOTg1ODN9.SGs0A3hDpcGOpAZ6hqmpWhVgSSzsHB4-_bkIW3ILAI4'
+    
+// valid locally
+let host = "http://localhost:8080";
+let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjlmYjdhMTU5MWU0IiwibmFtZSI6IlRoZSBCb3NzIiwiaWF0IjoxNjc5Mzk5OTE3fQ.H2t-duWyS8Z_sGdXp5I44aYwoBo8b49XFgf96z5k2Js'
+
+export { host };
+export { token };
