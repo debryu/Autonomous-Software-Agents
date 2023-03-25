7a671347e0b14989e676766d429a81eb10a1b9a5
Mateo-drr <mateodrr@gmail.com>
2023-03-25 16:39:35 +0000

    idfk

---
+++ config.js
@@ -0,0 +1,10 @@
+// valid on render
+// let host = "https://deliveroojs.onrender.com";
+// let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWFyY28iLCJpYXQiOjE2NzcxOTg1ODN9.SGs0A3hDpcGOpAZ6hqmpWhVgSSzsHB4-_bkIW3ILAI4'
+    
+// valid locally
+let host = "http://10.196.171.118:8080/";
+let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImY4NjhlODA2ZWVkIiwibmFtZSI6IldhbGwtRSIsImlhdCI6MTY3OTQwMDQ0OX0.qdL0Z7UkWiz4mG0kRpRR2Xqn4w4BdGi2dpEfK40zRFk'
+
+export { host };
+export { token };
